994eaa4e1877b7f7cede3359ca5dd8a672872ab1
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2025-04-01 10:06:52 -0500

    chore: initial commit

---
+++ src/main/java/com/example/consultoriafinanciera/ConsultoriaFinancieraApplication.java
@@ -0,0 +1,13 @@
+package com.example.consultoriafinanciera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultoriaFinancieraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultoriaFinancieraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/consultoriafinanciera/ConsultoriaFinancieraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultoriafinanciera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriaFinancieraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
